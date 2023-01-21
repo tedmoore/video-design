4c58b93e85af10cccc6b9f9986f3f79948155fe1
Ted Moore <ted@tedmooremusic.com>
2023-01-21 16:36:03 -0500

    WIP waveform-rects

--- src/Waveform.hpp
+++ src/Waveform.hpp
@@ -25,7 +25,7 @@ public:
     void receiveOSC(int width, int height, std::string label, float val) override;
     void displayWaveform(int wf_int, int x, int y, int z, float hmul2, int display_width, int display_height);
     
-    enum waveformType { NORM , LISSAJOUS , IKEDA };
+    enum waveformType { NORM , LISSAJOUS , IKEDA, RECTS };
     
     waveformType wfType = NORM;
     
@@ -46,4 +46,150 @@ public:
 //    bool lissajous = false;
     int maxNWaveforms = 2;
 };
+
+void Waveform::setup(int width, int height, float** waveforms_, int n_waveforms_, int length_, float** vecHistory, int vector_length, int history_length, bool vecHistoryFull, ofxYAML& config){
+    lissajous_line_width = config["modules"]["waveform"]["lissajous-line-width"].as<float>();
+    waveform_line_width = config["modules"]["waveform"]["waveform-line-width"].as<float>();
+    n_waveforms = n_waveforms_;
+    length = length_;
+    waveforms = waveforms_;
+    type = WAVEFORM;
+    h = height;
+    
+    xoff = new int[n_waveforms];
+    yoff = new int[n_waveforms];
+    zoff = new int[n_waveforms];
+    
+    hmul = new float[n_waveforms];
+    show = new bool[n_waveforms];
+    
+    show[0] = true;
+    xoff[0] = 0;
+    yoff[0] = height / 2;
+    zoff[0] = 0;
+    hmul[0] = 1;
+    
+//    bool lissajous = false;
+    //int maxNWaveforms = 2;
+    
+    newParams(width, height, vecHistory,vector_length, history_length, vecHistoryFull);
+}
+
+//void Waveform::update(){}
+
+void Waveform::display(int width, int height, int frame_num, std::unordered_map<std::string, float>* common_features, bool isNRT){
+        
+    switch(wfType){
+        case LISSAJOUS:
+        {
+            ofSetColor(255);
+            ofNoFill();
+            ofSetLineWidth(lissajous_line_width);
+            float w = width / 2.f;
+            ofBeginShape();
+            for (int i = 0; i < length * 0.1; i++) {
+                ofVertex(xoff[1] + w + (waveforms[0][i] * h), yoff[1] + (waveforms[1][i] * h));
+            }
+            ofEndShape();
+        }
+            break;
+        case NORM:
+        {
+            for (int i = 0; i < n_waveforms; i++) {
+                if (show[i]) {
+                    displayWaveform(i % maxNWaveforms, xoff[i], yoff[i], zoff[i], hmul[i],width,height);
+                }
+            }
+        }
+            break;
+        case IKEDA:
+        {
+            int w = width / n_waveforms;
+            float rect_height = (float)height / length;
+            ofSetColor(255,pow(common_features->at("loudness"),2) * 255); // what should the ikeda alpha be
+            ofSetLineWidth(0);
+            float runningsum = 0;
+            for (int i = 0; i < n_waveforms; i++) {
+                for(int y = 0; y < length; y++){
+                    float absval = abs(waveforms[i][y]);
+                    runningsum += absval;
+                    if(absval > ikeda_avg){
+                        ofDrawRectangle(w * i, y * rect_height, w, rect_height);
+                    }
+                }
+            }
+            
+            ikeda_avg = ofLerp(ikeda_avg, (runningsum / (n_waveforms * height)), 0.01);
+        }
+            break;
+        case RECTS:
+        {
+            // RECTS
+            float littleA = (width * height) / float(length);
+            int side = ceil(sqrt(littleA));
+            
+            int counter = 0;
+            for(int x = 0; x < width; x+= side){
+                for(int y = 0; y < height; y += side){
+                    ofSetColor(ofMap(waveforms[0][counter],-1.f,1.f,0,255));
+                    ofDrawRectangle(x, y, side, side);
+                }
+            }
+        }
+            break;
+    }
+}
+
+void Waveform::displayWaveform(int wf_int, int x, int y, int z, float hmul2, int display_width, int display_height){
+    ofSetColor(255,255,255,255);
+    ofNoFill();
+    ofSetLineWidth(waveform_line_width);
+    ofBeginShape();
+    float xhop = (float)display_width / (float)length;
+    for (int i = 0; i < length; i++) {
+      float y2 = y + (waveforms[wf_int][i] * h * hmul2);
+      float x2 = x + (i * xhop);
+      ofVertex(x2, y2, -z);
+    }
+    ofEndShape();
+}
+
+void Waveform::newParams(int width, int height, float** vecHistory, int vector_length, int history_length, bool vecHistoryFull){
+
+//    wfType = (waveformType)ofRandom(4.0);
+    wfType = RECTS;
+    
+    for (int i = 0; i < n_waveforms; i++) {
+        if (i > 0) {
+            xoff[i] = ofRandom(-width, width);
+            yoff[i] = ofRandom(0, height);
+            zoff[i] = ofRandom(0, height);
+            if (ofRandom(1.0) > 0.5) {
+                show[i] = true;
+            } else {
+                show[i] = false;
+            }
+        }
+        
+        hmul[i] = ofRandom(0.3, 1.0);
+    }
+}
+
+void Waveform::interact(VisualContent* other){}
+
+void Waveform::receiveOSC(int width, int height, std::string label, float val){
+    if(label == "setMaxNWaveforms"){
+          maxNWaveforms = int(val);
+    } else if (label == "setWaveformType"){
+        wfType = (waveformType)val;
+    } else if (label == "resetLissajousXY"){
+          xoff[1] = 0;
+          yoff[1] = height/2;
+    }
+}
+
+void Waveform::screenResize(int w, int h) {
+    yoff[0] = h / 2;
+}
+
 #endif /* Waveform_hpp */
